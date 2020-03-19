4839c4e9471336a2517c99d1a92d1a18e40a7c84
Mukul Jangid <49061120+mukul9070@users.noreply.github.com>
2020-03-19 14:17:38 -0400

    Comments
    Making the statements  more understandable

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
-#character mapping problem
+#one to one character mapping function
 def char_mapping(s1, s2):
-    
+    # edge case to eliminate unequal lengths
     if len(s1) != len(s2):
         return False
     
+    # mappings stored for checking later
     maps = {}
     for i in range(len(s1)):
         if s1[i] in maps:
             if maps[s1[i]] != s2[i]:
-                return False
+                return False # did not map
         else:
             maps[s1[i]] = s2[i]
-    return True
+    return True # no maps failed
             
 s1 = sys.argv[1]
 s2 = sys.argv[2]
