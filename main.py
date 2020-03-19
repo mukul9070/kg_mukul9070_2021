a88bea97cf30f4d11fd490259ab099aa50cb5256
Mukul Jangid <49061120+mukul9070@users.noreply.github.com>
2020-03-19 14:06:55 -0400

    Updated input taking style

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 #character mapping problem
 def char_mapping(s1, s2):
     
@@ -13,7 +14,7 @@ def char_mapping(s1, s2):
             maps[s1[i]] = s2[i]
     return True
             
-s1 = input()
-s2 = input()
+s1 = sys.argv[1]
+s2 = sys.argv[2]
 
 print(char_mapping(s1,s2))
